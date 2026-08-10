ef82a840496434f179e31e610b12d86da4f94934
Stanislav <stas.hanchev@gmail.com>
2026-08-10 23:05:04 +0300

    name required

--- src/models/user.js
+++ src/models/user.js
@@ -2,18 +2,18 @@ import { model, Schema } from 'mongoose';
 
 const userSchema = new Schema(
   {
-    name: { type: String, trim: true },
+    name: { type: String, trim: true, required: true },
     email: { type: String, unique: true, required: true, trim: true },
     password: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-userSchema.pre('save', function () {
-  if (!this.name) {
-    this.name = this.email;
-  }
-});
+// userSchema.pre('save', function () {
+//   if (!this.name) {
+//     this.name = this.email;
+//   }
+// });
 
 userSchema.methods.toJSON = function () {
   const obj = this.toObject();
